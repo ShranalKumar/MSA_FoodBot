34897f9caad97db722a6665aa2d0e795f5a095b3
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2017-11-23 09:19:43 +1300

    Wed 22 MSA

--- controller/LuisDialog.js
+++ controller/LuisDialog.js
@@ -1,16 +1,15 @@
 var builder = require('botbuilder');
-
+var food = require("./FavouriteFoods");
 
 exports.startDialog = function(bot) {
-
-    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/{YOUR_APP_ID_HERE}?subscription-key={YOUR_KEY_HERE}&timezoneOffset=0&q=');
+    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/e8413459-4ba3-4dbf-b41f-f98e34159da4?subscription-key=d1c174e41a47422aa30e74c7f2429503&verbose=true&timezoneOffset=0&q='); +
 
     bot.recognizer(recognizer);
 
     bot.dialog('WantFood', function(session, args) {
         if (!isAttachment(session)) {
             // Pulls out the food entity from the session if it exists
-            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'food');
+            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Food');
 
             // Checks if the food entity was found
             if (foodEntity) {
@@ -36,7 +35,7 @@ exports.startDialog = function(bot) {
         if (!isAttachment(session)) {
 
             // Pulls out the food entity from the session if it exists
-            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'food');
+            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Food');
 
             // Checks if the for entity was found
             if (foodEntity) {
@@ -52,21 +51,44 @@ exports.startDialog = function(bot) {
     });
 
     bot.dialog('GetFavouriteFood', [
-        // Insert favourite food logic here later
+        function(session, args, next) {
+            session.dialogData.args = args || {};
+            if (!session.conversationData["username"]) {
+                builder.Prompts.text(session, "Enter a username to setup your account.");
+            }
+        },
+        function(session, results, next) {
+
+            if (results.response) {
+                session.conversationData["username"] = results.response;
+            }
+
+            session.send("Retrieving your favourite foods");
+            food.displayFavouriteFood(session, session.conversationData["username"]); // <---- THIS LINE HERE IS WHAT WE NEED 
+        }
     ]).triggerAction({
         matches: 'GetFavouriteFood'
     });
 
-    bot.dialog('LookForFavourite', [
-        // Insert logic here later
-    ]).triggerAction({
+    bot.dialog('LookForFavourite', function(session, args) {
+        if (!isAttachment(session)) {
+            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Food');
+
+            if (foodEntity) {
+                session.send('Finding %s', foodEntity.entity);
+            } else {
+                session.send("No food identified! Please try again");
+            }
+        }
+    }).triggerAction({
         matches: 'LookForFavourite'
     });
 
 
-    bot.dialog('WelcomeIntent', [
-        // Insert logic here later
-    ]).triggerAction({
+    bot.dialog('WelcomeIntent', function(session, args) {
+        session.send("Hi there. Welcome to the Food Bot");
+        session.send("What would you like to do today?");
+    }).triggerAction({
         matches: 'WelcomeIntent'
     });
 }
